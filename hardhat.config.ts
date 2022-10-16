9128378f9dedd257420196225ee0d571bbd63a26
Steven Enamakel <enamakel@mahadao.com>
2022-10-16 10:12:39 +0100

    updated hardhat config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,85 +1,123 @@
-import 'solidity-coverage';
-import 'hardhat-gas-reporter';
-import '@nomiclabs/hardhat-web3';
-import '@nomiclabs/hardhat-ethers';
-import '@nomiclabs/hardhat-etherscan';
+import * as dotenv from "dotenv";
 
-require('dotenv').config();
+import { HardhatUserConfig, task } from "hardhat/config";
 
+import "@nomiclabs/hardhat-etherscan";
+import "@nomiclabs/hardhat-waffle";
+import "@typechain/hardhat";
+import "hardhat-gas-reporter";
+import "solidity-coverage";
 
-export default {
-  default: 'mainnet',
-  networks: {
-    hardhat: {},
-    mainnet: {
-      url: `https://mainnet.infura.io/v3/${process.env.INFURA_PROJECT_ID}`,
-      accounts: [process.env.METAMASK_WALLET_SECRET],
-      gasMultiplier: 1.2,
-      gasPrice: 100000000000,
-    },
-    ropsten: {
-      url: `https://ropsten.infura.io/v3/${process.env.INFURA_PROJECT_ID}`,
-      accounts: [process.env.METAMASK_WALLET_SECRET],
-    },
-    rinkeby: {
-      url: `https://rinkeby.infura.io/v3/${process.env.INFURA_PROJECT_ID}`,
-      accounts: [process.env.METAMASK_WALLET_SECRET],
-    },
-    bsc: {
-      url: 'https://bsc-dataseed.binance.org/',
-      accounts: [process.env.METAMASK_WALLET_SECRET],
-    },
-    matic: {
-      url:
-        'https://apis.ankr.com/0aa7b5a6761f4b87ae97c6b718d900ff/0a39ba8bf2c40d99b20fea4372ebaa68/polygon/full/main',
-      accounts: [process.env.METAMASK_WALLET_SECRET],
-      gasPrice: 50 * 1000000000, // 5.1 gwei
-    },
-    maticMumbai: {
-      url: 'https://matic-mumbai.chainstacklabs.com',
-      accounts: [process.env.METAMASK_WALLET_SECRET],
-    },
-    development: {
-      url: 'http://localhost:8545',
-      accounts: [process.env.METAMASK_WALLET_SECRET],
-    },
+dotenv.config();
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+const config: HardhatUserConfig & any = {
+  vyper: {
+    version: "0.2.7",
   },
   solidity: {
     compilers: [
       {
-        version: '0.8.0',
+        version: "0.8.4",
         settings: {
           optimizer: {
             enabled: true,
-            runs: 200,
+            runs: 100,
           },
         },
       },
       {
-        version: '0.8.1',
+        version: "0.7.6",
         settings: {
           optimizer: {
             enabled: true,
-            runs: 200,
+            runs: 100,
           },
         },
       },
-
-    ]
+      {
+        version: "0.5.0",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 100,
+          },
+        },
+      },
+    ],
   },
-  paths: {
-    sources: './contracts',
-    tests: './test',
-    cache: './build/cache',
-    artifacts: './build/artifacts',
+  networks: {
+    hardhat: {
+      forking: {
+        url: `https://eth-mainnet.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
+      },
+    },
+    dev: {
+      url: `http://127.0.0.1:8545/`,
+      accounts:
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
+    ropsten: {
+      url: `https://ropsten.infura.io/v3/${process.env.INFURA_PROJECT_ID}`,
+      accounts:
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
+    ethereum: {
+      url: `https://mainnet.infura.io/v3/${process.env.INFURA_PROJECT_ID}`,
+      accounts:
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
+    mumbai: {
+      url: "https://matic-mumbai.chainstacklabs.com",
+      accounts:
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
+    matic: {
+      url: "https://polygon-rpc.com/",
+      accounts:
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
+    rinkeby: {
+      url: `https://rinkeby.infura.io/v3/${process.env.INFURA_PROJECT_ID}`,
+      accounts:
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
+    goerli: {
+      url: `https://goerli.infura.io/v3/${process.env.INFURA_PROJECT_ID}`,
+      accounts:
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
+  },
+  abiExporter: {
+    path: "./output/abis/",
+    runOnCompile: true,
+    clear: true,
+    flat: true,
+    spacing: 2,
+    pretty: true,
   },
   gasReporter: {
-    currency: 'USD',
-    enabled: true,
+    enabled: process.env.REPORT_GAS !== undefined,
+    currency: "USD",
   },
   etherscan: {
-    // Your API key for Etherscan
-    // Obtain one at https://etherscan.io/
-    apiKey: process.env.ETH_API_KEY,
+    apiKey: {
+      mainnet: process.env.ETHERSCAN_API_KEY,
+      polygon: process.env.POLYGONSCAN_API_KEY,
+    },
   },
 };
+
+export default config;
