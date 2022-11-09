5ec3ed5b146a672a271d3c89893635b85ff74f54
Steven Enamakel <enamakel@mahadao.com>
2022-11-09 05:22:27 +0000

    added staking keeper

---
+++ scripts/deploy/stakingRewardsKeeper.ts
@@ -0,0 +1,39 @@
+import { BigNumber } from "ethers";
+import { ethers, network } from "hardhat";
+import { deployOrLoadAndVerify, getOutputAddress } from "../utils";
+
+async function main() {
+  console.log(`Deploying to ${network.name}...`);
+  const e18 = BigNumber.from(10).pow(18);
+
+  const [deployer] = await ethers.getSigners();
+  console.log(`Deployer address is ${deployer.address}.`);
+
+  const maha = await getOutputAddress("MAHA");
+  const sclp = await getOutputAddress("SCLP");
+
+  const mahaDistributor = await getOutputAddress("MAHAFeeDistributor");
+  const sclpDistributor = await getOutputAddress("SCLPFeeDistributor");
+
+  const keeper = await deployOrLoadAndVerify(
+    "StakingRewardsKeeper",
+    "StakingRewardsKeeper",
+    [
+      [mahaDistributor, sclpDistributor], // IFeeDistributor[] memory _distributors,
+      [maha, sclp], // IERC20[] memory _tokens,
+      [e18.mul(1000), e18.mul(9615)], // uint256[] memory _tokenRates,
+      maha, // IERC20 _maha,
+      e18.mul(10), // uint256 _mahaRewardPerEpoch
+    ]
+  );
+
+  console.log(
+    "next epoch is at",
+    new Date((await keeper.nextEpochPoint()) * 1000)
+  );
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
